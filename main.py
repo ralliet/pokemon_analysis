f95678e6cd2c5e33282092e1ff54d29cdd1a71ba
Ruben Alliet <allietruben@gmail.com>
2018-08-30 12:55:41 +0200

    read csv file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("123")
+#read CSV file
+import pandas as pd
+import re
+#statistical data visualization
+import seaborn as sb
+import matplotlib.pyplot as plt
+
+
+frame = pd.read_csv('../input/pokemon.csv')
+
+
+
